2d7c01d69a77c31decc5b89497a9af4b5f03dc4d
Luis <luis1@uni-bremen.de>
2018-12-25 12:42:21 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/OpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL.dir/main.cpp.o"
+  "../bin/OpenGL.pdb"
+  "../bin/OpenGL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
